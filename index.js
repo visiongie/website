3d04022cada9e58c07ce06bf431721955d69839f
baen <gececi1204@gmail.com>
2020-12-13 18:47:21 +0300

    😳 big changes

---
+++ index.js
@@ -0,0 +1,14 @@
+tippy('.inviteButton', {
+  animateFill: true,
+  content: 'Add bot to your server!'
+})
+
+tippy('.supportButton', {
+  animateFill: true,
+  content: 'Join support server!'
+})
+
+tippy('.githubButton', {
+  animateFill: true,
+  content: 'AshBot ♥️ open-source'
+})
